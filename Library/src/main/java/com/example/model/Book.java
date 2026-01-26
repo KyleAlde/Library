980b680a490376ce6851944b6b324bcdf8fc5a49
Jarod Kyle Alde <aldejarod@gmail.com>
2026-01-26 17:11:38 +0800

    Adjustments to Book model and DAO for synopsis

--- Library/src/main/java/com/example/model/Book.java
+++ Library/src/main/java/com/example/model/Book.java
@@ -11,13 +11,15 @@ public class Book {
     private String publisher;
     private LocalDate publicationDate;
     private BookStatus status;
+    private String synopsis;
     private Set<String> genres;
     private Set<String> availableFormats;
 
-    public Book(String isbn, String title, String author, String publisher, LocalDate publicationDate, BookStatus status) {
+    public Book(String isbn, String title, String author, String synopsis, String publisher, LocalDate publicationDate, BookStatus status) {
         this.isbn = isbn;
         this.title = title;
         this.author = author;
+        this.synopsis = synopsis;
         this.publisher = publisher;
         this.publicationDate = publicationDate;
         this.status = status;
@@ -28,6 +30,7 @@ public class Book {
     public String getIsbn() { return isbn; }
     public String getTitle() { return title; }
     public String getAuthor() { return author; }
+    public String getSynopsis() { return synopsis; }
     public String getPublisher() { return publisher; }
     public LocalDate getPublicationDate() { return publicationDate; }
     public BookStatus getStatus() { return status; }
