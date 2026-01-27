b711d25ccc776df43a589f056f2202731f082bd7
Jarod Kyle Alde <aldejarod@gmail.com>
2026-01-27 12:16:28 +0800

    Added searchBook() method

--- Library/src/main/java/com/example/utility/dao/CartDAO.java
+++ Library/src/main/java/com/example/utility/dao/CartDAO.java
@@ -6,12 +6,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.model.Book;
 import com.example.utility.DatabaseConnection;
 
 public class CartDAO {
     private final DatabaseConnection db = new DatabaseConnection();
-    private final BookDAO bookDAO = new BookDAO();
 
     //==============================================================
     //                           CREATE
